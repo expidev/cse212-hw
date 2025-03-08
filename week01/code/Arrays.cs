fdffcd07eaa5496083266551deef7132ecd50bb9
Nasandratra Harivonjy <nasandratra@soar.com>
2025-03-08 21:53:59 +0300

    week 01

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,17 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // initialize the array of size length to store the results
+        double[] result = new double[length];
 
-        return []; // replace this return statement with your own
+        // loop through the array and calculate the multiples of the number
+        // starting from index 0 = number * 1, index 1 = number * 2, index 2 = number * 3, etc.
+        for (int i = 0, start = 1; i < length; i++)
+        {
+            result[i] = number * start;
+            start++;
+        }
+        return result;
     }
 
     /// <summary>
@@ -25,9 +30,21 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        int length = data.Count;
+        int[] temp = new int[length];
+
+        // copy the data list to a temporary array
+        for (int i = 0; i < length; i++)
+        {
+            temp[i] = data[i];
+        }
+
+        // Rearrange data list from our copy
+        // The first index will be the new one based on the rotation
+        // Then iterate in a way that within the length but cyclic using modulo
+        for (int i = 0; i < length; i++)
+        {
+            data[i] = temp[(length + i - amount) % length];
+        }
     }
 }
